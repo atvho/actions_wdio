cf92e6e3102625471d93459ed779f72dfdace56f
axel <atvho@i.ua>
2024-04-19 21:19:26 +0300

    split second spec

--- test/specs/test.second.e2e.ts
+++ test/specs/test.second.e2e.ts
@@ -3,6 +3,10 @@ import LoginPage from '../pageobjects/login.page.js'
 import SecurePage from '../pageobjects/secure.page.js'
 
 describe('Second My Login application', () => {
+    it('should open the page', async () => {
+        await LoginPage.open()
+    })
+
     it('should login with invalid credentials', async () => {
         await LoginPage.open()
 
