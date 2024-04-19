c59f9b283859331bfb503457af6ef136578f209a
axel <atvho@i.ua>
2024-04-19 20:31:25 +0300

    adde second spec

--- test/specs/test.second.e2e.ts
+++ test/specs/test.second.e2e.ts
@@ -2,11 +2,11 @@ import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 import SecurePage from '../pageobjects/secure.page.js'
 
-describe('First My Login application', () => {
-    it('should login with valid credentials', async () => {
+describe('Second My Login application', () => {
+    it('should login with invalid credentials', async () => {
         await LoginPage.open()
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
+        await LoginPage.login('tomsmithth', 'SuperSecretPassword!')
         await expect(SecurePage.flashAlert).toBeExisting()
         expect(SecurePage.flashAlert).toHaveText(expect.stringContaining('You logged into a secure area!'))
     })
