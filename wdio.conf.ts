8f022be2577a133b1a2ceabb1dd44d81ce73bba8
axel <atvho@i.ua>
2024-04-19 20:49:45 +0300

    lock modified

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -30,8 +30,7 @@ export const config: Options.Testrunner = {
     // of the config file unless it's absolute.
     //
     specs: [
-        // './test/specs/**/*.ts'
-        './test/specs/test.second.e2e.ts'
+        './test/specs/**/*.ts'
     ],
     // Patterns to exclude.
     exclude: [
