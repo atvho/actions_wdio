db013f42e2eab546732c85d16bf9d2e853d7e3f2
Oleksii Dubchak <oleksii.dubchak@edvantis.com>
2024-04-25 18:28:54 +0300

    add verification file downloading

--- test/specs/test.export.e2e.ts
+++ test/specs/test.export.e2e.ts
@@ -1,8 +1,17 @@
-import { expect } from '@wdio/globals'
+// import { expect } from '@wdio/globals'
+import { expect } from 'chai'
 import DownloadPage from '../pageobjects/download.page.js'
 import SecurePage from '../pageobjects/secure.page.js'
+import fsExtra from 'fs-extra';
+const pathToChromeDownloads = './exportDownloads';
 
 describe('Fourth test export application', () => {
+    before(() => {
+        // Clean up the chromeDownloads folder and create a fresh one
+        fsExtra.removeSync(pathToChromeDownloads);
+        fsExtra.mkdirsSync(pathToChromeDownloads);
+    });
+
     it('should open download page', async () => {
         await DownloadPage.open()
     })
@@ -13,7 +22,19 @@ describe('Fourth test export application', () => {
         await listItem.waitForClickable()
         await listItem.click()
         await browser.pause(1000)
-        await expect(SecurePage.flashAlert).toBeExisting()
+        expect(await SecurePage.flashAlert.isExisting()).to.be.true
+    })
+
+    it('should verify if file is downloaded', async () => {
+        await browser.pause(1000)
+        let comparePath = `${pathToChromeDownloads}/screenshots.jpg`
+        let file = fsExtra.existsSync(comparePath)
+        console.log(`COMPARE PATH ${comparePath}`);
+        console.log(`FILE BOOLEAN ${file}`);
+        const createdFile = fsExtra.readdirSync(pathToChromeDownloads);
+        console.log(`CREATED FILE ${createdFile}`);
+        expect(createdFile[0]).to.have.string('.jpg');
+        
     })
 })
 
